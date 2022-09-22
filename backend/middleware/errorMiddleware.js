2ea1e5f5d862bbbd0813b9d3e292ccfa4a390a8b
Petya Angelova <bedfordpetya@gmail.com>
2022-09-22 20:24:45 +0300

    Goals controller and routes set up

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,15 @@
+//functions that execute on request when error override Express error handler
+const errorHandler = (err, req, res, next) =>{
+    const statusCode = res.statusCode ? res.statusCode : 500
+
+    res.status(statusCode)
+
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    })
+}
+
+module.exports = {
+    errorHandler,
+}
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,26 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
 
-// get goals
+//get goals and set goals in one line as both on the same root route
+router.route('/')
+    .get(getGoals)
+    .post(setGoal)
+//// get goals
+//router.get('/', getGoals)
+////create goals
+//router.post('/', setGoal)
 
-router.get('/', (req, res) =>{
-    res.status(200).json({message: 'Get goals'})
-})
-
-//create goals
-router.post('/', (req, res) =>{
-    res.status(200).json({message: 'Set goal'})
-})
-
-//change goal select by id, we can access with params
-router.put('/:id', (req, res) =>{
-    res.status(200).json({message: `Update goal ${req.params.id}`})
-})
-
-//delete goal select by id, we can access with params
-router.delete('/:id', (req, res) =>{
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
-})
+//change goal and delete goal as both on sama route
+router.route('/:id')
+    .delete(deleteGoal)
+    .put(updateGoal)
+// //change goal select by id, we can access with params
+// router.put('/:id', updateGoal)
+// //delete goal select by id, we can access with params
+// router.delete('/:id', deleteGoal)
 
 
 module.exports = router

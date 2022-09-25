172fbdceb90ea142696ee9e0bfcebc8b82bf0625
Petya Angelova <bedfordpetya@gmail.com>
2022-09-25 15:03:24 +0300

    spinner added

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
-import { register, reset } from '../features/auth/authSlice'
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from '../components/Spinner'
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -16,16 +17,52 @@ function Register() {
 
   const { name, email, password, password2 } = formData;
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+    useEffect( () => {  
+      if(isError) {
+        toast.error(message)
+      }
+
+      if(isSuccess || user){
+        navigate('/')
+      }
+
+      dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+  
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-    }))
+    }));
   };
 
   const onSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    if (password !== password2) {
+      toast.error("Password do not match");
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      dispatch(register(userData));
+    }
   };
+
+  if(isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
@@ -38,7 +75,6 @@ function Register() {
 
       <section className="form">
         <form onSubmit={onSubmit}>
-          
           {/************** FORM NAME *************************/}
           <div className="form-group">
             <input
@@ -99,7 +135,6 @@ function Register() {
               Submit
             </button>
           </div>
-
         </form>
       </section>
     </>

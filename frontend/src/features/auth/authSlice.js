36d8709f0168469df1d5392b31cb0db30b699326
Petya Angelova <bedfordpetya@gmail.com>
2022-09-25 16:11:41 +0300

    login added

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -30,6 +30,24 @@ export const register = createAsyncThunk(
   }
 );
 
+//Login user
+export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    return thunkAPI.rejectWithValue(message);
+  }
+});
+
+//LOGOOUT
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout();
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -43,20 +61,44 @@ export const authSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder
-        .addCase(register.pending, (state) => { //when pending
-            state.isLoading = true
-        })
-        .addCase(register.fulfilled, (state, action) => { //when fulfilled 
-            state.isLoading = false
-            state.isSuccess = true
-            state.user = action.payload
-        })
-        .addCase(register.rejected, (state, action) =>{ //when rejected
-            state.isLoading = false
-            state.isError = true
-            state.message = action.payload
-            state.user = null
-        })
+      .addCase(register.pending, (state) => {
+        //when register pending
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        //when register fulfilled
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        //when register rejected
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(login.pending, (state) => {
+        //when login pending
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        //when login fulfilled
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        //when login rejected
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+        state.user = null;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        //on logout
+        state.user = null;
+      });
   },
 });
 
